08d5c8c3b5d0ac357b18f3c13b24d4126ae320c8
Aayush <aayushtiwari160@gmail.com>
2024-12-07 17:54:53 +0530

    Initial commit

---
+++ Revision_Project/src/main/java/com/app/prac/RevisionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.app.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionProjectApplication.class, args);
+	}
+
+}
